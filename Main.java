1b25c9f26bcc4acfe6bb6e7e1b5aba0142d6af32
AniA16051 <anirudh16.ashok@gmail.com>
2026-03-16 09:08:21 +0530

    UC4: RoomAvailabilitySRCH

--- Main.java
+++ Main.java
@@ -1,26 +1,16 @@
 import java.util.Map;
 class BookMyStay {
     public static void main(String[] args) {
-        System.out.println("Hotel Room Inventory Status\n");
+        System.out.println("Room Search\n");
 
         RoomInventory inventory = new RoomInventory();
-        Map<String, Integer> availability = inventory.getRoomAvailability();
-
         Room single = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suite = new SuiteRoom();
 
-        System.out.println("Single Room:");
-        single.displayRoomDetails();
-        System.out.println("Available Rooms: " + availability.get("Single Room") + "\n");
+        RoomSearchService searchService = new RoomSearchService();
 
-        System.out.println("Double Room:");
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + availability.get("Double Room") + "\n");
-
-        System.out.println("Suite Room:");
-        suite.displayRoomDetails();
-        System.out.println("Available Rooms: " + availability.get("Suite Room"));
+        searchService.searchAvailableRooms(inventory, single, doubleRoom, suite);
     }
 
 }
