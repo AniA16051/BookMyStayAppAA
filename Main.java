aa236f5aeb3f4d321c3c81abfff30677e064692c
Ani16051 <anirudh16.ashok@gmail.com>
2026-03-16 08:41:43 +0530

    Base appuc1

---
+++ Main.java
@@ -0,0 +1,15 @@
+class BookMyStay {
+
+    /**
+     * Application entry point.
+     * * This method is the first method executed
+     * * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Display the welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+
+        // Confirm system initialization
+        System.out.println("System initialized successfully.");
+    }
+}
